1d882572bb77d1347bb25bdb4e2fc331aba5008b
Josh Newkirk <newkirk.josh@gmail.com>
2013-04-14 12:20:10 -0400

    Merge commit

--- project/src/com/copilot/app/IncidentActivity.java
+++ project/src/com/copilot/app/IncidentActivity.java
@@ -1,26 +1,89 @@
 package com.copilot.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
 
 import com.actionbarsherlock.app.SherlockExpandableListActivity;
 
-public class IncidentActivity extends SherlockExpandableListActivity{
+public class IncidentActivity extends SherlockExpandableListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 	}
-	
+
 	@Override
 	protected void onStart() {
-		// TODO Auto-generated method stub
 		super.onStart();
 	}
-	
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
-		// TODO Auto-generated method stub
 		super.onSaveInstanceState(outState);
+	}
+
+	class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+		private Context context;
+		private String[] groups;
+		private List<Fragment> children;
+
+		public ExpandableListAdapter(Context context) {
+			this.context = context;
+			this.groups = context.getResources().getStringArray(
+					R.array.incident_group_titles);
+			children = new ArrayList<Fragment>();
+		}
+
+		public Object getChild(int groupPosition, int childPosition) {
+			return null;
+		}
+
+		public long getChildId(int groupPosition, int childPosition) {
+			return childPosition;
+		}
+
+		public View getChildView(int groupPosition, int childPosition,
+				boolean isLastChild, View convertView, ViewGroup parent) {
+			View v = null;
+			return v;
+		}
+
+		public int getChildrenCount(int groupPosition) {
+			return children.size();
+		}
+
+		public Object getGroup(int groupPosition) {
+			return groups[groupPosition];
+		}
+
+		public int getGroupCount() {
+			return groups.length;
+		}
+
+		public long getGroupId(int groupPosition) {
+			return groupPosition;
+		}
+
+		public View getGroupView(int groupPosition, boolean isExpanded,
+				View convertView, ViewGroup parent) {
+			View v = null;
+			return v;
+		}
+
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		public boolean isChildSelectable(int groupPosition, int childPosition) {
+			return true;
+		}
 	}
 }
